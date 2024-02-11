1cba01c5ad0f57169ddbc18484b50a687dec97d9
ElTinmar <martin.privat@hotmail.fr>
2024-02-11 20:42:58 +0100

    update

--- plot.py
+++ plot.py
@@ -2,6 +2,8 @@ from xrayutilities.io.rigaku_ras import RASFile
 import matplotlib.pyplot as plt
 from pathlib import Path
 
+# TODO Phi have the greek letter (degree)
+
 # Folder that contains the .ras files
 DATADIR = '/home/martin/Documents/Lea_DRX/'
 
@@ -29,11 +31,20 @@ for path in Path(DATADIR).rglob('*.ras'):
     # result file 
     resultfile = Path(RESULTDIR).joinpath(path.with_suffix('.svg').name)
 
+    if path.match('*100*'):
+        col = '#ff3333'
+    elif path.match('*110*'):
+        col = '#33ff33'
+    elif path.match('*111*'):
+        col = '#3333ff'
+    else:
+        col = '#000000'
+        
     # do the plotting
     f = plt.figure()
-    plt.plot(file.scan.data[axis], intensities)
+    plt.plot(file.scan.data[axis], intensities, color=col)
     plt.xlabel(axis)
-    plt.ylabel('Intensity')
+    plt.ylabel('Intensity (counts)')
     plt.yscale('log') # use log scale for the Y axis
     plt.title(path.name)
     plt.savefig(resultfile)
